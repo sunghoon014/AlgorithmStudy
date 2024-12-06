fbff69d6ecef0195b97961008b2aaf4187680376
suvinn <ssongsuvin@naver.com>
2024-12-06 11:08:33 +0900

    11279 ssb

---
+++ Data_Structure/Heap/baekjoon_11279_ssb.py
@@ -0,0 +1,13 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+n = int(input())
+
+heap = []
+for _ in range(n):
+    data = int(input())
+    if data == 0:
+        print(-heapq.heappop(heap) if heap else 0)
+    else:
+        heapq.heappush(heap, -data)
