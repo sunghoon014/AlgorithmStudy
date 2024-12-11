f6245281f30f502b9430f4d7f08c0ccfad69b3dd
suvinn <ssongsuvin@naver.com>
2024-12-11 15:09:39 +0900

    10815 ssb

---
+++ Search/Binary_Search/baekjoon_10815_ssb.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+a.sort()
+result = []
+for num in b:
+    if a[bisect_left(a, num)]==num:
+        result.append('1')
+    else:
+        result.append('0')
+
+print(' '.join(result))
