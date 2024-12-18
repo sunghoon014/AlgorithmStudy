fad1d76db6100b20889e993cdcdf1c356b1dddbe
김현욱 <soccering17@gmail.com>
2024-12-18 09:45:14 +0900

    hwk

--- Search/BFS, DFS/baekjoon_4179_불!/baekjoon_4179_hwk.py
+++ Search/BFS, DFS/baekjoon_4179_불!/baekjoon_4179_hwk.py
@@ -24,22 +24,19 @@ dcs = [1, -1, 0, 0]
 def BFS(que, visited):
     while que:
         r, c = que.popleft()
+        if grid[r][c]=="J" and (r==0 or r==N-1 or c==0 or c==M-1):    # 지훈이 가장자리에 도달하여 탈출
+            return visited[r][c]
         for i in range(4):
             nr = r+drs[i]
             nc = c+dcs[i]
             if 0<=nr<N and 0<=nc<M and grid[nr][nc]=='.':
                 if grid[r][c] == "J":   # 지훈이일 경우
                     grid[nr][nc] = "J"
-                    visited[nr][nc] = visited[r][c]+1
-                    if nr==0 or nr==N-1 or nc==0 or nc==M-1:    # 탈출
-                        return visited[nr][nc]
+                    visited[nr][nc] = visited[r][c]+1    # 이동한 시간 기록하면서 가기
                 elif grid[r][c]=="F":   # 불일 경우
                     grid[nr][nc] = "F"
-                que.append((nr, nc))
-
+                que.append((nr, nc))    # 지훈이든 불이든 상관 없이 추가함. grid로 구별할거니까 여기서 구별할 필요 없음
+    # 중간에 return 못하면 탈출 못한거니까 불가능 반환
     return "IMPOSSIBLE"
 
-if J_init[0]==0 or J_init[0]==N-1 or J_init[1]==0 or J_init[1]==M-1:
-    print(1)
-else:
-    print(BFS(queue, visited))
+print(BFS(queue, visited))
