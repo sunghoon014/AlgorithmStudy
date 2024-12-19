f39710ddda75b79b367f3f705772578d45cb83e7
suvinn <ssongsuvin@naver.com>
2024-12-19 14:49:21 +0900

    15651 ssb

---
+++ Back_Tracking/baekjoon_15651_ssb.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+stack = []
+def dfs():
+    if len(stack)==m:
+        print(*stack)
+        return
+    for i in range(1, n+1):
+        stack.append(i)
+        dfs()
+        stack.pop()
+
+dfs()
