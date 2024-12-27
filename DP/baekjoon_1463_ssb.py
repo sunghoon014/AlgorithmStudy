83658285dbc34bffb8b9cc68399438886ad0751d
suvinn <ssongsuvin@naver.com>
2024-12-27 16:52:39 +0900

    1463 ssb

---
+++ DP/baekjoon_1463_ssb.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+d = [0]*(n+1)
+for i in range(2, n+1):
+    if i%3==0 and i%2==0:
+        d[i] = min(d[i//3]+1, d[i//2]+1, d[i-1]+1)
+    elif i%3==0:
+        d[i] = min(d[i//3]+1, d[i-1]+1)
+    elif i%2==0:
+        d[i] = min(d[i//2]+1, d[i-1]+1)
+    else:
+        d[i] = d[i-1]+1
+
+print(d[n])
